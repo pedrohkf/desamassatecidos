79654e36d8a60997893f0fc58550d8e67a5cfb2d
Pedro Henrique <phkfaria@gmail.com>
2024-01-11 12:44:44 -0300

    desamassatecidos

---
+++ script.js
@@ -0,0 +1,25 @@
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
+
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+ 
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
